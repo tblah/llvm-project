680d0908e22b7923ec09afebf751392ad75fdd6a
Jack Styles <jack.styles@arm.com>
2025-12-17 11:42:46 +0000

    Updates to bounds rework
    - Force the first 3 entries to the StructArg to be the bounds info
    - Ensure it will work when executing the tasks in parallel

--- llvm/include/llvm/Frontend/OpenMP/OMPIRBuilder.h
+++ llvm/include/llvm/Frontend/OpenMP/OMPIRBuilder.h
@@ -14,6 +14,7 @@
 #ifndef LLVM_FRONTEND_OPENMP_OMPIRBUILDER_H
 #define LLVM_FRONTEND_OPENMP_OMPIRBUILDER_H
 
+#include "llvm/ADT/SetVector.h"
 #include "llvm/Frontend/Atomic/Atomic.h"
 #include "llvm/Frontend/OpenMP/OMPConstants.h"
 #include "llvm/Frontend/OpenMP/OMPGridValues.h"
@@ -2364,6 +2365,7 @@ public:
     PostOutlineCBTy PostOutlineCB;
     BasicBlock *EntryBB, *ExitBB, *OuterAllocaBB;
     SmallVector<Value *, 2> ExcludeArgsFromAggregate;
+    SetVector<Value *> Inputs, Outputs;
 
     /// Collect all blocks in between EntryBB and ExitBB in both the given
     /// vector and set.
